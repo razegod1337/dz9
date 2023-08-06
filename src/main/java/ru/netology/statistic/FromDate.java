e3eb01b34e8fbd586297e971b0247dbebdc30957
razegod1337 <serzh.zhdanov19@gmail.com>
2023-08-06 21:49:00 +0300

    First

---
+++ src/main/java/ru/netology/statistic/FromDate.java
@@ -0,0 +1,7 @@
+package ru.netology.statistic;
+
+public class FromDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/netology/statistic/Post.java
@@ -0,0 +1,10 @@
+package ru.netology.statistic;
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FromDate birthday;
+}
